923b8af7986c4b65e89e6103930c077fcb0e84df
informatykaw21 <informatyk.aw21@gmail.com>
2024-02-19 10:39:16 +0100

    create view

---
+++ snowflake_view1.sql
@@ -0,0 +1,17 @@
+Create or replace  view if not exists product_performance_view AS
+
+SELECT
+p.id
+, p.name
+, p.price
+, p.category
+, sum(o.quantity) as sum_quantity
+, count(o.o_id) as count_orders
+from products p 
+inner join  orders o
+on p.product_id = o.product_id 
+group by 1,2,3,4
+order by 5 desc 
+;
+
+select * from product_performance_view
